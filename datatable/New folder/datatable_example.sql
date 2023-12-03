388d0fc44a6577890363baf5f0413d2747c8d78c
Prashanth-2001 <106829953+Prashanth-2001@users.noreply.github.com>
2023-12-03 12:21:41 +0530

    Update datatable_example.sql

--- datatable/New folder/datatable_example.sql
+++ datatable/New folder/datatable_example.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `datatable_example`
+-- Database: `datatable_db`
 --
 
 -- --------------------------------------------------------
